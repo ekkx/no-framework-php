dbd094e8283a11948fd6768ec731dce413e6303b
ekkx <geniusnaru@gmail.com>
2024-07-15 14:21:41 +0900

    feat: add config object which converts variables from .env to php object

---
+++ app/Config.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class Config
+{
+    private static Config $instance;
+
+    public bool $appDebug;
+
+    public string $logFileDebug;
+    public string $logFileInfo;
+    public string $logFileError;
+
+    private function __construct()
+    {
+        $config = static::getConfig();
+
+        foreach ($config as $key => $value) {
+            $this->{$key} = $value;
+        }
+    }
+
+    private static function getConfig(): array
+    {
+        return [
+            "appDebug" => isset($_ENV["APP_DEBUG"]) && strtolower($_ENV["APP_DEBUG"]) === "true",
+
+            "logFileDebug" => $_ENV["LOG_FILE_DEBUG"] ?? "php://stdout",
+            "logFileInfo" => $_ENV["LOG_FILE_INFO"] ?? "php://stdout",
+            "logFileError" => $_ENV["LOG_FILE_ERROR"] ?? "php://stderr",
+        ];
+    }
+
+    public static function instance(): Config
+    {
+        if (!isset(static::$instance)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+}
