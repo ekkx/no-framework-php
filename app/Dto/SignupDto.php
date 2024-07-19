22aebd582edd54585f6d24784ae966e9eef5f368
ekkx <geniusnaru@gmail.com>
2024-07-19 18:33:12 +0900

    feat: add sign up dto

---
+++ app/Dto/SignupDto.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dto;
+
+use App\Core\Dto;
+use App\Core\Exception\ValidationException;
+
+class SignupDto extends Dto
+{
+    public ?string $username = null;
+    public ?string $email = null;
+    public ?string $password = null;
+    public ?string $passwordConfirm = null;
+
+    /**
+     * @throws ValidationException
+     */
+    public function __construct(array $data)
+    {
+        $this->validate($data, [
+            "required" => [
+                ["username"],
+                ["email"],
+                ["password"],
+                ["passwordConfirm"],
+            ],
+            "email" => [
+                ["email"]
+            ],
+            "lengthMin" => [
+                ["password", 6]
+            ],
+            "equals" => [
+                ["passwordConfirm", "password"],
+            ],
+        ]);
+        $this->setProperties($data);
+    }
+}
