ef7bc9b64cb7092859acc4cc7d178bb17dc3a0fc
ekkx <geniusnaru@gmail.com>
2024-07-19 18:41:50 +0900

    fix: fix admin controller

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -6,18 +6,17 @@ namespace App\Controller;
 
 use App\Core\Context;
 use App\Core\Http\StatusCode;
-use App\Model\User;
 
 class AdminController
 {
     public function index(Context $ctx): void
     {
-         $ctx->res->render(StatusCode::OK, "admin/index.twig", [
-             "user" => new User([
-                 "username" => "Username",
-                 "email" => "your@email.com",
-                 "password" => "*********"
-             ])
-         ]);
+        $ctx->res->render(StatusCode::OK, "admin/index.twig", [
+            "user" => [
+                "username" => "Username",
+                "email" => "your@email.com",
+                "password" => "*********"
+            ]
+        ]);
     }
 }
