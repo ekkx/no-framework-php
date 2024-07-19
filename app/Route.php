0e427b007685748eec1a552c357bab1486083d2c
ekkx <geniusnaru@gmail.com>
2024-07-19 18:31:19 +0900

    feat: update routes

--- app/Route.php
+++ app/Route.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Controller\AdminController;
+use App\Controller\Api\UserApiController;
+use App\Controller\AuthController;
 use App\Controller\ErrorController;
 use App\Controller\HomeController;
 use App\Core\Exception\InternalServerErrorException;
@@ -17,6 +20,21 @@ class Route
     public static function init(Kernel $app): void
     {
         Router::get("/", HomeController::class, "index");
+
+        Router::group("/auth", function () {
+            Router::get("/signup", AuthController::class, "signup");
+            Router::get("/login", AuthController::class, "login");
+        });
+
+        Router::get("/admin", AdminController::class, "index");
+
+        Router::group("/api", function () {
+            Router::group("/users", function () {
+                Router::post("/login", UserApiController::class, "login");
+                Router::post("/create", UserApiController::class, "create");
+                Router::post("/logout", UserApiController::class, "logout");
+            });
+        });
 
         $app->onError(NotFoundException::class, ErrorController::class, "notFound");
         $app->onError(MethodNotAllowedException::class, ErrorController::class, "methodNotAllowed");
