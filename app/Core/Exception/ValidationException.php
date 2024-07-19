9af4ba150c1a63e20187962b130fb1173a9b8f10
ekkx <geniusnaru@gmail.com>
2024-07-19 18:29:41 +0900

    refactor: dto validation

---
+++ app/Core/Exception/ValidationException.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Exception;
+
+use Throwable;
+
+class ValidationException extends BaseException
+{
+    protected array $errors;
+
+    public function __construct(array $errors, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->errors = $errors;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
