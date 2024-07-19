7fd9abaa1582b3aa8f2d723bfbfdb751148c2d78
ekkx <geniusnaru@gmail.com>
2024-07-19 18:30:07 +0900

    feat: add exceptions for authentication

---
+++ app/Exception/InvalidEmailOrPasswordException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use App\Core\Exception\BaseException;
+
+class InvalidEmailOrPasswordException extends BaseException
+{
+
+}
---
+++ app/Exception/UserAlreadyRegisteredException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use App\Core\Exception\BaseException;
+
+class UserAlreadyRegisteredException extends BaseException
+{
+
+}
